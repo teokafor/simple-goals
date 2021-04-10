869cf1ea5b579e77df1ef3f6ad3029ca2a585761
Tobe Okafor <teokafor@students.unwsp.edu>
2021-04-09 23:58:38 -0500

    Added goal description column to db and corresponding functions

--- client/projectio.py
+++ client/projectio.py
@@ -7,21 +7,21 @@ cur = con.cursor()  # Create a cursor object
 
 # TODO: close connection to db at some point!
 # For the Goal table:
-# This creates an empty goal with an automatically generated ID
-def new_goal(goal_name):
+# This creates an empty goal with an automatically generated ID.
+def new_goal(goal_name, goal_desc):
     cur.execute("SELECT * FROM Goal")
     rows = cur.fetchall()  # Get the rows from the table
     goal_id = len(rows)  # The primary key GoalID is simply the index of the new row
-    sqlite_insert_with_param = "INSERT INTO Goal (GoalID, GoalName, StartDate, EndDate, TimeSpent, Completion) VALUES (?, ?, ?, ?, ?, ?);"
-    data_tuple = (goal_id, goal_name, 'N/A', 'N/A', 0, 0)
+    sqlite_insert_with_param = "INSERT INTO Goal (GoalID, GoalName, GoalDesc, StartDate, EndDate, TimeSpent, Completion) VALUES (?, ?, ?, ?, ?, ?, ?);"
+    data_tuple = (goal_id, goal_name, goal_desc, 'N/A', 'N/A', 0, 0)
     cur.execute(sqlite_insert_with_param, data_tuple)  # Create a row with the given information
     con.commit()  # Insert the new row
 
 
 # Modify any existing goal. I might break this up into several smaller functions so you don't need to pass every column.
-def modify_goal(goal_name, start_date, end_date, time_spent, completion, goal_id):
-    sqlite_insert_with_param = "UPDATE Goal SET GoalName = ?, StartDate = ?, EndDate = ?, TimeSpent = ?, Completion = ? WHERE GoalID = ? ;"
-    data_tuple = (goal_name, start_date, end_date, time_spent, completion, goal_id)
+def modify_goal(goal_name, goal_desc, start_date, end_date, time_spent, completion, goal_id):
+    sqlite_insert_with_param = "UPDATE Goal SET GoalName = ?, GoalDesc = ?, StartDate = ?, EndDate = ?, TimeSpent = ?, Completion = ? WHERE GoalID = ? ;"
+    data_tuple = (goal_name, goal_desc, start_date, end_date, time_spent, completion, goal_id)
     cur.execute(sqlite_insert_with_param, data_tuple)  # Modify the row linked to GoalID.
     con.commit()
 
