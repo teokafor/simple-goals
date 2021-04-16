6284c89fa37aacc9b91fc55ae22d2f8dc18a8a19
Tobe Okafor <teokafor@students.unwsp.edu>
2021-04-16 00:17:02 -0500

    Implemented basic subgoal support

--- client/projectio.py
+++ client/projectio.py
@@ -1,3 +1,4 @@
+# TODO: close connection to db at some point!
 import sqlite3
 
 # Initial connection to database
@@ -5,7 +6,7 @@ con = sqlite3.connect('db/main.db')  # Connect to the main database file
 cur = con.cursor()  # Create a cursor object
 cur.execute("SELECT * FROM Goal")
 rows = cur.fetchall()  # Get the rows from the table
-
+cur.execute('PRAGMA foreign_keys=ON')  # Enforce foreign keys. Needed to link subgoals to goals table.
 
 class Row:
     def __init__(self, goal_id):
@@ -13,7 +14,7 @@ class Row:
         cur.execute("SELECT * FROM Goal WHERE GoalID = ?", (self.__goal_id,))
         row = cur.fetchone()
 
-        # Assign variables from row data
+        # Assign goal table variables from row data
         self.__goal_name = row[1]
         self.__goal_desc = row[2]
         self.__start_date = row[3]
@@ -21,12 +22,7 @@ class Row:
         self.__time_spent = row[5]
         self.__completion = row[6]
 
-    # Apparently, you cannot edit primary keys with sqlite. Oops!
-   # def set_goal_id(self, new_goal_id):
-       # self.__goal_id = new_goal_id
-       # self.update_row()
-
-    # Setters
+    # Setters for goal table
     def set_goal_name(self, goal_name):
         self.__goal_name = goal_name
         self.update_row()
@@ -51,7 +47,7 @@ class Row:
         self.__completion = completion
         self.update_row()
 
-    # Getters
+    # Getters for goal table
     def get_goal_id(self): return self.__goal_id
     def get_goal_name(self): return self.__goal_name
     def get_goal_desc(self): return self.__goal_desc
@@ -61,36 +57,46 @@ class Row:
     def get_completion(self): return self.__completion
 
     def update_row(self):
-        sqlite_insert_with_param = "UPDATE Goal SET GoalName = ?, GoalDesc = ?, StartDate = ?, EndDate = ?, TimeSpent = ?, Completion = ? WHERE GoalID = ? ;"
+        insert = "UPDATE Goal SET GoalName = ?, GoalDesc = ?, StartDate = ?, EndDate = ?, TimeSpent = ?, Completion = ? WHERE GoalID = ? ;"
         data_tuple = (self.__goal_name, self.__goal_desc, self.__start_date, self.__end_date, self.__time_spent, self.__completion, self.__goal_id)
-        cur.execute(sqlite_insert_with_param, data_tuple)  # Modify the row linked to GoalID.
+        cur.execute(insert, data_tuple)  # Modify the row linked to GoalID.
         con.commit()
 
+    # Subgoal-related functions:
+    # Create a new subgoal.
+    def new_subgoal(self, subgoal_name):
+        insert = "INSERT INTO SubGoal (GoalID, SubName, Completion) VALUES (?, ?, ?);"
+        tuple = (self.__goal_id, subgoal_name, 0)
+        cur.execute(insert, tuple)
+        con.commit()
 
-# TODO: close connection to db at some point!
-# For the Goal table:
+    # Retrieve all subgoals associated with this goal.
+    def get_subgoals(self):
+        insert = "SELECT * FROM Subgoal WHERE GoalID = ?"
+        data_tuple = (self.__goal_id,)
+        cur.execute(insert, data_tuple)
+        subgoals = cur.fetchall()
+        for subgoal in subgoals:
+            print(subgoal)
+
+
+# General functions not tied to the Row class:
 # This creates an empty goal with an automatically generated ID.
 def new_goal(goal_name, goal_desc):
     cur.execute("SELECT * FROM Goal")
 
-    # Goal ID generation. The new ID will be the largest in the table + 1.
-    largest = 0
-    for row in rows:
-        if row[0] > largest:
-            largest = row[0]
-    goal_id = (largest + 1)
-
     # Create and commit new row.
-    sqlite_insert_with_param = "INSERT INTO Goal (GoalID, GoalName, GoalDesc, StartDate, EndDate, TimeSpent, Completion) VALUES (?, ?, ?, ?, ?, ?, ?);"
-    data_tuple = (goal_id, goal_name, goal_desc, 'N/A', 'N/A', 0, 0)
-    cur.execute(sqlite_insert_with_param, data_tuple)  # Create a row with the given information
+    insert = "INSERT INTO Goal (GoalName, GoalDesc, StartDate, EndDate, TimeSpent, Completion) VALUES (?, ?, ?, ?, ?, ?);"
+    data_tuple = (goal_name, goal_desc, 'N/A', 'N/A', 0, 0)
+    cur.execute(insert, data_tuple)  # Create a row with the given information
     con.commit()  # Insert the new row
 
 
 # Bare-bones delete function.
 def delete_goal(goal_id):
-    cur.execute("DELETE FROM Goal WHERE GOALID = ?", (goal_id,))
+    # Delete subgoals first
+    cur.execute("DELETE FROM Subgoal WHERE GoalID = ?", (goal_id,))
+
+    # Now we can safely delete the goal itself
+    cur.execute("DELETE FROM Goal WHERE GoalID = ?", (goal_id,))
     con.commit()
-
-
-# For the SubGoal table:
