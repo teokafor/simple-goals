80caba3ff00022d4bd282abf8b10abfcdcf0f766
Tobe Okafor <teokafor@students.unwsp.edu>
2021-04-19 14:26:00 -0500

    added subgoals Listbox functionality

--- client/screen/home.py
+++ client/screen/home.py
@@ -24,15 +24,13 @@ class HomePage(gui.Frame):
 
         # Description Label and StringVar
         self.description_var = gui.StringVar()
-        self.description_label = ttk.Label(self, textvariable=self.description_var)
-        self.description_label.grid(row=13, column=3)
+        self.description_label = ttk.Label(self, textvariable=self.description_var, wraplength=140)
+        self.description_label.grid(row=12, column=1)
 
         # Listbox for Goals table
         self.goals_listbox = gui.Listbox(self, height=15, width=25, selectmode=gui.SINGLE)
         self.goals_listbox.grid(row=5, column=1)
-        self.goals_listbox.bind('<<ListboxSelect>>', self.retrieve_subgoals)
-        self.goals_listbox.bind('<<ListboxSelect>>', self.update_description)
-
+        self.goals_listbox.bind('<<ListboxSelect>>', self.on_goal_select)
 
         # Scrollbar for goals Listbox
         self.goals_v_scrollbar = gui.Scrollbar(self, orient='vertical', command=self.goals_listbox.yview)
@@ -42,8 +40,8 @@ class HomePage(gui.Frame):
         # Populate goals Listbox on startup
         self.update_goals_listbox()
 
-        # Blank widgets to space out window. This is terrible and should (probably) be removed ;)
-        self.blank1 = ttk.Label(self, text='                            ')
+        # Blank widget to space out window. This is terrible and should (probably) be removed ;)
+        self.blank1 = ttk.Label(self, text=' ', width=20)
         self.blank1.grid(row=0, column=3)
 
         # TODO: Only make add subgoal button visible/press-able when a goal is selected.
@@ -71,20 +69,23 @@ class HomePage(gui.Frame):
         for goal in goals:
             self.goals_listbox.insert(gui.END, goal.get_goal_name())
 
-    def retrieve_subgoals(self, event):
+    # Run this function when the user selects a goal in the Listbox
+    def on_goal_select(self, event):
+        # First, update the subgoals Listbox
+        self.subgoals_listbox.delete(0, gui.END)
         index = self.goals_listbox.curselection()
         goals = projectio.make_object_list()
         subgoals = goals[index[0]].get_subgoals()
         for subgoal in subgoals:
-            print(subgoal[1])
+            self.subgoals_listbox.insert(gui.END, subgoal[1])
+
+        # Then we can update the goal's description
+        index = self.goals_listbox.curselection()
+        goals = projectio.make_object_list()
+        self.description_var.set(goals[index[0]].get_goal_desc())
 
     # This function is used to get row data of currently selected Listbox item
     def get_selected_goal_id(self):
         index = self.goals_listbox.curselection()
         goals = projectio.make_object_list()
         return goals[index[0]].get_goal_id()
-
-    def update_description(self, event):
-        index = self.goals_listbox.curselection()
-        goals = projectio.make_object_list()
-        self.description_var.set(goals[index[0]].get_goal_desc())
