ecc606cffcb8642fe4dc5a517ac3eb30b8e5489a
Draylar <samuelhimes@gmail.com>
2021-04-20 20:05:38 -0500

    add new goal page and update main.py to have new_goal/home methods for traversing UI

--- client/main.py
+++ client/main.py
@@ -3,22 +3,35 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 from client.screen.home import Ui_MainWindow as HomeWindow
+from client.screen.new_goal import Ui_NewGoalWindow as NewGoalWindow
+
+APPLICATION = QApplication(sys.argv)
+ROOT = QMainWindow()
+HOME = HomeWindow()
+
+
+def home():
+    """
+    Opens the "Home" window.
+    This window instance is global, so state will persist between screen changes.
+    """
+    HOME.setupUi(ROOT)
 
 
 def new_goal():
-    print("Opening new goal GUI.")
+    """
+    Opens a new "Add Goal" window.
+    A new instance is created each time this method is called, which means user-entered values will not persist.
+    """
+    new_goal_window = NewGoalWindow()
+    new_goal_window.setupUi(ROOT)
 
 
 if __name__ == '__main__':
-    # Initialize our application and create a root window.
-    app = QApplication(sys.argv)
-    root = QMainWindow()
-
     # Create an instance of our home window and assign button-click events.
-    home = HomeWindow()
-    home.setupUi(root)
-    home.newGoal.clicked.connect(new_goal)
+    HOME.setupUi(ROOT)
+    HOME.newGoal.clicked.connect(new_goal)
 
     # Display the root window.
-    root.show()
-    sys.exit(app.exec_())
+    ROOT.show()
+    sys.exit(APPLICATION.exec_())
